fbe54790b33af3a13ba5eef463b0624eb20c048e
Vic Sergeev <133888588+vsrov161@users.noreply.github.com>
2026-04-16 13:06:40 +0300

    init
    spring project imported

---
+++ src/main/java/com/vicsergeev/WalletManagerApi/WalletManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.vicsergeev.WalletManagerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vicsergeev/WalletManagerApi/WalletManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicsergeev.WalletManagerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
